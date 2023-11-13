a1aa64a68b1f58a987daaaf8d5f12ca6aa73f10f
Esen Meredow <esca656585@gmail.com>
2023-11-13 17:07:00 +0500

    updated

---
+++ public/js/script.js
@@ -0,0 +1,124 @@
+const x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
+
+function returnPosition(position) {
+    return position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+        x.innerHTML = "User denied the request for Geolocation."
+        break;
+        case error.POSITION_UNAVAILABLE:
+        x.innerHTML = "Location information is unavailable."
+        break;
+        case error.TIMEOUT:
+        x.innerHTML = "The request to get user location timed out."
+        break;
+        case error.UNKNOWN_ERROR:
+        x.innerHTML = "An unknown error occurred."
+        break;
+    }
+}
+
+function start() {
+    
+    $.ajax({
+        url: 'http://ip-api.com/json/217.174.228.202',
+        type: 'GET',
+        success: function (response) {
+            console.log(response);
+            $("#success").html('ip address: ' + response.query);
+            $("#lat").val(response.lat);
+            $("#lon").val(response.lon);
+            console.log(response);
+        },
+        error: function(err) {
+            console.log(err);
+            $("#error").html(err.statusText);
+            console.log(err);
+        }
+    });
+
+    var path = window.location.origin;
+    var token = '6|2ApWl5xJqso1J3w7NHUFuRklqnLhXDl5WlJdyIBz';
+    var data = "lat=37.91363186773645&lon=58.355444373933715&tarif_id=1";
+
+    const successCallback = (position) => {
+        console.log(position);
+    };
+    
+    const errorCallback = (error) => {
+        console.log(error);
+    };
+      
+    var coord = navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+    console.log(coord);
+
+    $.ajax({
+        headers: {
+            Authorization: 'Bearer ' + token
+        },
+        url: path + '/api/travel-start',
+        type: 'POST',
+        data: data,
+        success: function (data) {
+            console.log(data);
+            $("#success").html(data);
+            console.log(data);
+            console.log(path);
+        },
+        error: function(err) {
+            console.log(err);
+            $("#error").html(err);
+            console.log(err);
+        }
+    });
+}
+
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const token = document.getElementById("token");
+
+    
+    var path = window.location.origin;
+    var data = 'username=' + username + '&password=' + password;
+    
+    $.ajax({
+        url: path + '/api/login',
+        type: 'POST',
+        data: data,
+        success: function (response) {
+            console.log(data);
+            $("#success").html(data);
+            $("#token").val(response.access_token);
+            console.log(data);
+            console.log(path);
+        },
+        error: function(err) {
+            console.log(err);
+            $("#error").html(err);
+            console.log(err);
+        }
+    });
+}
+
+function finish() {
+    alert('finish');
+}
--- resources/views/distance.blade.php
+++ resources/views/distance.blade.php
@@ -3,22 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://api/mapbox.com/mapbox-gl-js/v1.8.0/mapbox-gl.css">
-    <title>Map</title>
-    <style>
-        body {
-            margin: 0;
-        }
-        #map {
-            width: 100vw;
-            height: 100vh;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>M'S Taxi</title>
 </head>
 <body>
+    <div class="d-flex justify-content-center align-items-center mh-100" style="height: 800px; flex-direction: column;">
     
-    <div id="map2"></div>
+        <label for="username">username</label>
+        <input type="text" id="username">
+        
+        <label for="password">password</label>
+        <input type="password" id="password">
 
-    <script src="{{ asset('map.js') }}" type="module"></script>
+        <button class="btn btn-primary m-3" onclick="login()">Login</button>
+        
+        <label for="token">token</label>
+        <input type="text" id="token" style="width: 500px;">
+
+        <hr />
+
+        <button class="btn btn-success w-25 m-3" onclick="start()">Start</button>
+
+        <button class="btn btn-danger w-25 m-3" onclick="finish()">Finish</button>
+        
+        <input type="text" name="lat" id="lat">
+
+        <input type="text" name="lon" id="lon">
+
+        <p id="location">Location</p>
+
+        <p style="color: green;" id="success"></p>
+        
+        <p style="color: red;" id="error"></p>
+
+        <div>
+            <h1>HTML Geolocation</h1>
+            <p>Click the button to get your coordinates.</p>
+
+            <button onclick="getLocation()">Try It</button>
+
+            <p id="demo"></p>
+        </div>
+    </div>
+
+    
+
+    <script src="{{ asset('metronic-template/v7/assets/js/ajax/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
