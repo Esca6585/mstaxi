449936f932d113fa3dbb9a13c261daa780b24e3d
Esen Meredow <esca656585@gmail.com>
2023-11-23 15:35:44 +0500

    remove laravel scout

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
 
 class Travel extends Model
 {
-    use HasFactory, Searchable;
+    use HasFactory;
 
     protected $table = 'travels';
 
