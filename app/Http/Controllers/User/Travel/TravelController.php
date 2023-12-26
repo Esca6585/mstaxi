1e4da8af462e4ed99ae576a4a0f18b742c2385fe
Esen Meredow <esca656585@gmail.com>
2023-12-26 17:31:01 +0500

    updated

--- app/Http/Controllers/User/Travel/TravelController.php
+++ app/Http/Controllers/User/Travel/TravelController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Location\Coordinate;
 use Location\Distance\Vincenty;
+use App\Models\User;
 use App\Models\Route;
 use App\Models\Travel;
 use App\Models\Tarif;
